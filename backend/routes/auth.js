311134ecae13ae18c8f7a81a525d0c3c6864efb0
Panjavit <panjavit.o@ku.th>
2025-06-12 18:08:11 +0700

    Create login and register API

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,18 @@
-import express from 'express';
-import bcrypt from 'bcryptjs';
-import { PrismaClient } from '@prisma/client';
+import express from 'express'; //สำหรับการสร้าง Router
+import bcrypt from 'bcryptjs'; //เข้ารหัสรหัสผ่าน
+import { PrismaClient } from '@prisma/client'; //ใช้เพื่อเชื่อมต่อและส่งคำสั่งไปยังฐานข้อมูล PostgreSQL
+import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
 const router = express.Router();
 
 // @route   POST /api/auth/register
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res) => { //req (Request),res (Response)  ใช้ async เพราะต้องมีการรอผลลัพธ์
     try {
         const { name, email, password, role } = req.body;
 
         // **ข้อควรระวัง** ฟอร์มสาธารณะไม่ควรอนุญาตให้สมัครเป็น 'admin'
-        if (role === 'admin') {
+        if (role === 'admin') { //Never Trust the Client
             return res.status(403).json({ message: 'ไม่สามารถลงทะเบียนเป็น Admin ได้' });
         }
         
@@ -21,7 +22,7 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({ message: 'อีเมลนี้ถูกใช้งานแล้ว' });
         }
         
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10); //ไม่เก็บรหัสผ่านจริง
 
         // Prisma จะสร้าง User และ Profile ที่เชื่อมกันใน Transaction เดียว (Nested Write)
         const newUser = await prisma.user.create({
@@ -47,5 +48,46 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// @route   POST /api/auth/login
+router.post('/login', async (req, res) =>{
+    try {
+        const {email,password} = req.body;
+        const user = await prisma.user.findUnique({
+            where: { email }
+        });
+        if (!user || !user.password){
+            return res.status(400).json({ message: 'อีเมลหรือรหัสผ่านไม่ถูกต้อง'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch){
+            return res.status(400).json({ message: 'อีเมลหรือรหัสผ่านไม่ถูกต้อง'})
+        }
+
+        //สร้างข้อมูลสำหรับ Token
+        const payload = {
+            user: {
+                id: user.id,
+                profileId: user.profileId,
+                role: user.role
+            }
+        };
+
+        //สร้างและส่ง Token
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: '1d'},
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+});
 // (ใส่ Route สำหรับ Login และ Social Login ต่อไป)
 export default router;
