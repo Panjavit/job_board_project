16a06a450a939ebe3d10331099341e6b0ffa28fd
Panjavit <panjavit.o@ku.th>
2025-06-18 20:21:59 +0700

    refactor(db): Reset migrations and refactor schema for interaction model

--- backend/prisma/migrations/20250618064148_init/migration.sql
+++ backend/prisma/migrations/20250618064148_init/migration.sql
@@ -1,9 +1,6 @@
 -- CreateEnum
 CREATE TYPE "Role" AS ENUM ('CANDIDATE', 'COMPANY', 'ADMIN');
 
--- CreateEnum
-CREATE TYPE "ApplicationStatus" AS ENUM ('APPLIED', 'COMPANY_INTERESTED');
-
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
@@ -21,13 +18,17 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "CandidateProfile" (
     "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
     "fullName" TEXT NOT NULL,
     "contactEmail" TEXT NOT NULL,
+    "desiredPosition" TEXT,
     "lineUserId" TEXT,
-    "education" TEXT,
+    "major" TEXT,
+    "studyYear" INTEGER,
     "bio" TEXT,
     "youtubeIntroLink" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "CandidateProfile_pkey" PRIMARY KEY ("id")
 );
@@ -35,35 +36,43 @@ CREATE TABLE "CandidateProfile" (
 -- CreateTable
 CREATE TABLE "CompanyProfile" (
     "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
     "companyName" TEXT NOT NULL,
     "about" TEXT,
     "contactInstructions" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "CompanyProfile_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Job" (
+CREATE TABLE "Skill" (
     "id" TEXT NOT NULL,
-    "companyProfileId" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "jobCategoryCode" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
 
-    CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Skill_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Application" (
+CREATE TABLE "UserSkill" (
     "id" TEXT NOT NULL,
-    "jobId" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
     "candidateProfileId" TEXT NOT NULL,
-    "applicationCode" TEXT NOT NULL,
-    "status" "ApplicationStatus" NOT NULL DEFAULT 'APPLIED',
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "skillId" TEXT NOT NULL,
 
-    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "UserSkill_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Interaction" (
+    "id" TEXT NOT NULL,
+    "contactInstructions" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "companyProfileId" TEXT NOT NULL,
+    "studentProfileId" TEXT NOT NULL,
+
+    CONSTRAINT "Interaction_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -79,7 +88,10 @@ CREATE UNIQUE INDEX "CandidateProfile_userId_key" ON "CandidateProfile"("userId"
 CREATE UNIQUE INDEX "CompanyProfile_userId_key" ON "CompanyProfile"("userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Application_applicationCode_key" ON "Application"("applicationCode");
+CREATE UNIQUE INDEX "Skill_name_key" ON "Skill"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSkill_candidateProfileId_skillId_key" ON "UserSkill"("candidateProfileId", "skillId");
 
 -- AddForeignKey
 ALTER TABLE "CandidateProfile" ADD CONSTRAINT "CandidateProfile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -88,10 +100,13 @@ ALTER TABLE "CandidateProfile" ADD CONSTRAINT "CandidateProfile_userId_fkey" FOR
 ALTER TABLE "CompanyProfile" ADD CONSTRAINT "CompanyProfile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Job" ADD CONSTRAINT "Job_companyProfileId_fkey" FOREIGN KEY ("companyProfileId") REFERENCES "CompanyProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "UserSkill" ADD CONSTRAINT "UserSkill_candidateProfileId_fkey" FOREIGN KEY ("candidateProfileId") REFERENCES "CandidateProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Application" ADD CONSTRAINT "Application_jobId_fkey" FOREIGN KEY ("jobId") REFERENCES "Job"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "UserSkill" ADD CONSTRAINT "UserSkill_skillId_fkey" FOREIGN KEY ("skillId") REFERENCES "Skill"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Application" ADD CONSTRAINT "Application_candidateProfileId_fkey" FOREIGN KEY ("candidateProfileId") REFERENCES "CandidateProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Interaction" ADD CONSTRAINT "Interaction_companyProfileId_fkey" FOREIGN KEY ("companyProfileId") REFERENCES "CompanyProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Interaction" ADD CONSTRAINT "Interaction_studentProfileId_fkey" FOREIGN KEY ("studentProfileId") REFERENCES "CandidateProfile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
