2752af12d208195c7eb71f43dcd6896799d97e2a
Panjavit <panjavit.o@ku.th>
2025-06-23 16:05:00 +0700

    feat: Integrate frontend project structure

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// components/index.ts
-export { default as ProfileHeader } from './ProfileHeader';
-export { default as ProfileCard } from './ProfileCard';
-export { default as SkillsCard } from './SkillsCard';
-export { default as FileUploadCard } from './FileUploadCard';
-export { default as VideoUploadCard } from './VideoUploadCard';
